1f6750cc0fe2adf72788588b99b070f8a39b5d69
johnnyrayalt <johnnyrayalt@gmail.com>
2018-05-04 12:29:03 -0700

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { masterArticlesArchive } from "./seed-data";
+import { Article } from "./models/article.model";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,11 @@ import { masterArticlesArchive } from "./seed-data";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  title = 'LOS ANGELES TIMES CLONE';
+  articles = [];
+  constructor() {
+    for(let article of masterArticlesArchive) {
+      this.articles.push(new Article(article.headline, article.images, article.blurb, article.datePublished, article.author));
+    }
+  }
 }
