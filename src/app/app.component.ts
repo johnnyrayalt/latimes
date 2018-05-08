f1530297b9908f0f986dc263faab2d5362a2e95f
johnnyrayalt <johnnyrayalt@gmail.com>
2018-05-08 13:10:31 -0700

    removed unnecesarry code that was causing rendering issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { masterArticlesArchive } from "./seed-data";
-import { Article } from "./models/article.model";
-import { ArticleCardComponent}  from "./article-card/article-card.component";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,15 +6,4 @@ import { ArticleCardComponent}  from "./article-card/article-card.component";
 })
 export class AppComponent{
   title = 'LOS ANGELES TIMES CLONE';
-  articles = [];
-  constructor() {
-    for(let article of masterArticlesArchive) {
-      this.articles.push(new Article(article.headline, article.images, article.blurb, article.datePublished, article.author));
-      console.log(article);
-    }
-  }
-
-  displayArticle(article) {
-    console.log(article.headline);
-  }
 }
