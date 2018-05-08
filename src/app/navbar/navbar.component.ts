465d36a09ca3b9ae0f482acad85c9ec647a1b962
johnnyrayalt <johnnyrayalt@gmail.com>
2018-05-08 15:16:17 -0700

    added navbar component with basic html structure

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
