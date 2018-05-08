3f7356d0964f2fe9f908151cac4aa9a4469fcfbe
johnnyrayalt <johnnyrayalt@gmail.com>
2018-05-08 13:08:36 -0700

    added seed data to ngOnInit and cleaned uphtml and css accordingly

--- src/app/article-card/article-card.component.ts
+++ src/app/article-card/article-card.component.ts
@@ -1,25 +1,52 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Article } from "./../models/article.model";
-import { AppComponent } from './../app.component';
-import { masterArticlesArchive } from '../seed-data';
+
 @Component({
   selector: 'app-article-card',
   templateUrl: './article-card.component.html',
   styleUrls: ['./article-card.component.css']
 })
 export class ArticleCardComponent implements OnInit {
-  @Input() article: Article[];
-  @Output() showArticle = new EventEmitter();
-
-  displayArticle() {
-    // for(let currentArticle of this.article) {
-      // this.showArticle.push(new Article(article.headline, article.images, article.blurb, article.datePublished, article.author));
-  }
-  ngOnInit() {
-    for(let currentArticle of masterArticlesArchive) {
-      this.showArticle.emit(currentArticle)
-      console.log(currentArticle.headline);
+  article: Article = {
+    headline: '',
+    images: '',
+    blurb: '',
+    datePublished: Date,
+    author: {
+      name: '',
+      photo: '',
+      bio: ''
     }
+  };
+
+  articles: Article[];
+
+  ngOnInit() {
+    this.articles = [
+      {
+        headline: 'Lorem ipsum dolor sit amet',
+        images: 'https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/02/13/104279122-IMG_0426.530x298.jpg?v=1487013108',
+        blurb:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        datePublished: new Date(),
+        author: {
+          name: 'Johnny Alt',
+          photo: 'http://frederatorblogs.com/random/files/2008/04/b-corrupted-by-random-noise-bit-error-rate0020.thumbnail.gif',
+          bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        }
+      },
+      {
+        headline: 'Lorem ipsum dolor sit amet',
+        images: 'https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/02/13/104279122-IMG_0426.530x298.jpg?v=1487013108',
+        blurb:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        datePublished: new Date('2/13/1991'),
+        author: {
+          name: 'Johnny Alt',
+          photo: 'http://frederatorblogs.com/random/files/2008/04/b-corrupted-by-random-noise-bit-error-rate0020.thumbnail.gif',
+          bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        }
+      }
+    ]
+
   }
 
 }
